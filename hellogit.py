2819beacf7080f3c96142ec67b0c366c5dee39e8
EduardoNavarreteB <enavarretebencomo@gmail.com>
2024-06-18 11:59:48 -0600

    se resolvieron los cambios

--- hellogit.py
+++ hellogit.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print("ENw Hello Github!")
-=======
-print("new heello giiitttt!")
->>>>>>> 8051233182181e57006393a91171d108dae35eae
--- login.py
+++ login.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print("login v2")
-=======
-print("login")
->>>>>>> 8051233182181e57006393a91171d108dae35eae
